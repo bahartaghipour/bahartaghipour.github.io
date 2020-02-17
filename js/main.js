ada9e8707bc9e066fbc4806eee0f420535d10a52
taghipourb <taghipourb@TAGHIPOURB>
2020-02-17 12:26:28 +0330

    first commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+window.onload = () => {
+  'use strict';
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./sw.js');
+  }
+
+  fetch('https://jsonplaceholder.typicode.com/photos')
+  .then(response => response.json())
+  .then(json => {
+    var items = json.slice(0,10);
+    albumMaker(items);
+
+    document.getElementById("search_input").addEventListener('keyup', function(e){
+      var value = e.target.value;
+      var album_items = items.filter(item => item.title.includes(value));
+      albumMaker(album_items);
+    })
+  });
+}
+function albumMaker(items) {
+  var album = '';
+  for(var i=0; i <= items.length - 1; i++){
+    album += '<div class="card"><img src="' + items[i].thumbnailUrl + '" /><div class="card-title">'+ items[i].title +'</div></div>';
+  };
+  document.getElementById("album").innerHTML = album;
+}
+
+Notification.requestPermission(function(status) {
+    console.log('Notification permission status:', status);
+    displayNotification()
+});
+
+function displayNotification() {
+  if (Notification.permission == 'granted') {
+    navigator.serviceWorker.getRegistration().then(function(reg) {
+      var options = {
+        body: 'Here is a notification body!',
+        icon: '../images/hello-icon-128.png',
+        vibrate: [100, 50, 100]
+      };
+      reg.showNotification('Hello world!', options);
+    });
+  }
+}
---
+++ sw.js
@@ -0,0 +1,60 @@
+var cacheVersion = '2';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/main.js',
+  '/favicon.ico'
+];
+currentCache = {
+  static: "static-cache-v" + cacheVersion,
+  dynamic: "dynamic-cache-v" + cacheVersion
+}
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(currentCache['static']).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', e => {
+  let expectedCacheNames = Object.values(currentCache);
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.forEach(cacheName => {
+          if(! expectedCacheNames.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    // caches.open(currentCache['dynamic']).then(cache => {
+    //   return cache.match(e.request).then(response => {
+    //     if(response)
+    //     return response;
+    //     return fetch(e.request).then(networkResponse => {
+    //       cache.put(e.request, networkResponse.clone());
+    //       return networkResponse;
+    //     });
+    //   });
+    // })
+    caches.match(e.request).then(response => {
+      if(response) return response;
+      return fetch(e.request).then(networkResponse => {
+        caches.open(currentCache['dynamic']).then(cache => {
+          cache.put(e.request, networkResponse.clone());
+          return networkResponse;
+        })
+      })
+    })
+  );
+});
